c68da8fe822d494558f686a6f13e001f48dd5a3d
ikitsmark <ikitsmark@gmail.com>
2025-03-11 19:54:52 +0100

    4.1 done, Vue.cli is active

--- vue-start-spa/src/App.vue
+++ vue-start-spa/src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <navbarComp 
+    :pages="pages" 
+    :active-page="activePage" 
+    :nav-link-click="(index)=>activePage = index"
+        ></navbarComp>
+
+    <page-viewer 
+    :page="pages[activePage]"
+    ></page-viewer>
+    <p>simple text</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavbarComp from './components/NavbarComp.vue';
+import PageViewer from './components/PageViewer.vue';
+
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: {
+        NavbarComp,
+        PageViewer
+    },
+    data() {
+                return {
+                    activePage: 0,
+                    pages: [
+                        {
+                            link: { text: 'Főoldal', url: 'home.html' },
+                            pageTitle: 'Fő Oldal',
+                            content: 'Ez itt a főoldal.'
+                        },
+                        {
+                            link: { text: 'Kapcsolat', url: 'contact.html' },
+                            pageTitle: 'Kapcsolat',
+                            content: 'A kapcsolatokat tartalmazó oldal.'
+                        },
+                        {
+                            link: { text: 'Rólam', url: 'about.html' },
+                            pageTitle: 'Rólam',
+                            content: 'Ez az oldal rólam szól.'
+                        },
+                    ]
+                };
+            },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ vue-start-spa/src/components/NavbarLink.vue
@@ -0,0 +1,29 @@
+<template>
+        <a 
+        class="nav-link" 
+        :class="activeClasses" 
+        aria-current="page" 
+        :href="page.link.url"
+        :title="`Ez a gomb a ${page.link.text}-ra/re navigál.`">
+        {{page.link.text }}
+                            </a>
+</template>
+
+<script>
+export default{
+    props:['page', 'isActive'],
+    computed:{
+        activeClasses() {
+            return {
+                active: this.isActive,
+                emphasize: this.isActive
+            };
+        }
+    }
+}
+</script>
+
+<style scoped>
+.emphasize {
+    text-decoration: underline !important
+}</style>
